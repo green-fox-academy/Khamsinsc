e1c34008c5a0edddfdead87060b3ee9d52a1926f
Khamsinsc <senbce@gmail.com>
2019-01-17 15:27:07 +0100

    next 3 files

---
+++ week-01/day-4/introduce-yourself.ts
@@ -0,0 +1,18 @@
+'use strict';
+
+// Write a program that prints a few details to the console about you
+// It should print each detail to a new line.
+//  - Your name
+//  - Your age
+//  - Your height in meters (as a decimal fraction)
+//
+//  Example output:
+//  John Doe
+//  31
+//  1.87
+
+
+console.log("hi all, my name is James Tang");
+console.log("my age is "+33);
+console.log("I\'ve no idea about my height, it should be around "+175+" cm");
+
