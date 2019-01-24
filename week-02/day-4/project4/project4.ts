754cfa6c39896c7edc199b1138787126bc936a5c
Khamsinsc <senbce@gmail.com>
2019-01-24 11:46:05 +0100

    last one for the week

--- week-02/day-4/project4/project4.ts
+++ week-02/day-4/project4/project4.ts
@@ -3,19 +3,27 @@
 const canvas = document.querySelector('.main-canvas') as HTMLCanvasElement;
 const ctx = canvas.getContext('2d');
 
-// hexagon
-var numberOfSides = 6,
-    size = 20,
-    Xcenter = 25,
-    Ycenter = 25;
+let hiveSize: number = 13,
+    numberOfSides: number = 6,
+    size: number = 25,
+    moveDistance: number = size * Math.sqrt(3),
+    xCoord: number,
+    yCoord: number;
 
-ctx.beginPath();
-ctx.moveTo (Xcenter +  size * Math.cos(0), Ycenter +  size *  Math.sin(0));          
-
-for (var i = 1; i <= numberOfSides;i += 1) {
-    ctx.lineTo (Xcenter + size * Math.cos(i * 2 * Math.PI / numberOfSides), Ycenter + size * Math.sin(i * 2 * Math.PI / numberOfSides));
+function drawPoly(x: number, y: number) {
+    ctx.beginPath();
+    ctx.moveTo(x + size * Math.cos(0), y + size * Math.sin(0));
+    for (let i: number = 1; i <= numberOfSides; i += 1) {
+        ctx.lineTo(x + size * Math.cos(i * 2 * Math.PI / numberOfSides), y + size * Math.sin(i * 2 * Math.PI / numberOfSides));
+    }
+    ctx.stroke();
 }
-ctx.stroke();
 
-
-
+for (let indexL: number = 0; indexL < hiveSize; indexL++) {
+    xCoord = (indexL * 1.5 * size) + canvas.width / 2 - (hiveSize - 1) / 2 * 1.5 * size;
+    yCoord = (moveDistance / 2 * (3 - Math.abs((indexL + 1) - ((hiveSize + 1) / 2)))) + canvas.height / 2 + (hiveSize - 2) / 2 * moveDistance;
+    for (let indexC = 0; indexC < hiveSize - Math.abs((indexL + 1) - ((hiveSize + 1) / 2)); indexC++) {
+        yCoord = yCoord - moveDistance;
+        drawPoly(xCoord, yCoord);
+    }
+}
