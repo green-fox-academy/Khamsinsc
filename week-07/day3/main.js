ef87266c1f865e3c60c559ab4bb7efc8bc5c19e8
Khamsinsc <senbce@gmail.com>
2019-03-06 13:03:28 +0100

    finished xhr ex1

--- week-07/day3/main.js
+++ week-07/day3/main.js
@@ -5,8 +5,8 @@ const content = document.querySelector('#content');
 const httpRequest = new XMLHttpRequest();
 httpRequest.onreadystatechange = console.log;
 
-button.addEventListener('click', ()=>{
-  httpRequest.open('GET', 'https://api.giphy.com/v1/gifs/search?api_key=NmI34IfzFvH9qlxWG0hZTlnUdfZhocZK&q=taylor+swift&limit=20&offset=0&rating=G&lang=en', true);
+button.addEventListener('click', () => {
+  httpRequest.open('GET', 'https://api.giphy.com/v1/gifs/search?api_key=NmI34IfzFvH9qlxWG0hZTlnUdfZhocZK&q=recursion&limit=50&offset=0&rating=G&lang=en', true);
   httpRequest.onload = () => {
     let responseData = JSON.parse(httpRequest.responseText);
     console.log(responseData.data[0]);
@@ -15,7 +15,8 @@ button.addEventListener('click', ()=>{
   httpRequest.send();
 })
 
-const renderHTML = (data)=>{
-  
-  content.insertAdjacentHTML('beforeend',`<img src=${data[0].images.preview_gif.url}>`)
+const renderHTML = (data) => {
+  for (let i = 0; i < data.length; i++) {
+    content.insertAdjacentHTML('beforeend', `<img src=${data[i].images.preview_gif.url}>`);
+  }
 }
