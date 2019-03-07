b38fa5a8f37c801961691baf51e2c79b1faf0e5e
Khamsinsc <senbce@gmail.com>
2019-03-07 11:13:46 +0100

    fatal errors solved, happy now

--- week-07/day4/matreview/server.js
+++ week-07/day4/matreview/server.js
@@ -1,17 +1,19 @@
 'use strict';
-
+/* require('dotenv').config(); */
 const express = require('express');
 const app = express();
 const PORT = 3000;
 const mysql = require('mysql');
+app.use(express.json());
 
 const conn = mysql.createConnection({
   host: 'localhost',
   user: 'root',
-  password: 'password'
+  password: 'password',
+  database: 'mysql',
 })
 
-conn.connect((err) => {
+ conn.connect((err) => {
   if (err) {
     console.log('Error connecting to DB');
     return;
@@ -19,7 +21,23 @@ conn.connect((err) => {
   console.log('DB is connected');
 })
 
-conn.end();
+app.get('/', (req, res) => {
+  conn.query('SELECT * FROM db;', (err, rows) => {
+    if (err) {
+      console.log(err);
+      res.status(500).send();
+      return;
+    }
+    res.send(rows);
+    console.log(rows);
+  })
+  conn.end(); 
+})
+
+app.post('/newtask', (req,res)=>{
+  conn.query(`INSERT INTO tasks (title,done) VALUES
+  ()`)
+})
 
 app.listen(PORT, () => {
   console.log(`Server is running at ${PORT}`);
