5667b53b2471e83069957d32aac314a1e1cd4196
Khamsinsc <senbce@gmail.com>
2019-03-08 14:49:51 +0100

    imported data and corrected the getpost

--- week-07/Reddit/server.js
+++ week-07/Reddit/server.js
@@ -7,7 +7,7 @@ const PORT = 8080;
 const mysql = require('mysql');
 const fs = require('fs');
 let postFile = JSON.parse(fs.readFileSync('./assets/posts.json', 'utf-8'));
-console.log(postFile.posts[0]);
+app.use(express.json());
 
 const connection = mysql.createConnection({
   host: 'localhost',
@@ -20,25 +20,25 @@ app.use(express.json());
 app.use('/assets', express.static('assets'));
 
 app.get('/hello', (req, res) => {
-  res.send(`Hello World!`);
+  res.send(`Hello world!`);
 })
-
 
 app.get('/posts', (req, res) => {
-  postFile.posts.forEach(post => {
-    connection.query(`insert into reddit set ?`, post, (err, result) => {
-
-    })
-  });
-  res.sendFile(path.join(__dirname, '/assets/posts.json'));
+  connection.query(`SELECT * FROM posts`, (err, result) => {
+    err ? console.log(err) : res.send(result);
+  })
 })
 
 
-
-
-
-
-
+app.post('/posts'), (req, res) => {
+  console.log(req);
+  res.send(`req`);
+  /* postFile.posts.forEach(post => {
+    connection.query(`SELECT * FROM posts`, (err, result) => {
+      err ? console.log(err) : console.log(result);
+    })
+  }); */
+}
 
 
 app.listen(PORT, () => {
