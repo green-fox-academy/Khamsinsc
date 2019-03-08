8e2806e4281f9e0e1951b592b2421f1aa5ac2a08
Khamsinsc <senbce@gmail.com>
2019-03-08 09:10:43 +0100

    day start

--- week-07/day4/personalDB/try.js
+++ week-07/day4/personalDB/try.js
@@ -1,7 +1,37 @@
 'use strict';
 const fs = require('fs');
+const express = require('express');
+const app = express();
 app.use(express.json());
 app.use('/static', express.static('static'));
-let file = fs.readFileSync('./static/database.csv');
-let jsonFile = JSON.parse(file);
-console.log(jsonFile);
+let file = fs.readFileSync('./static/database.csv', 'utf-8');
+
+
+//var csv is the CSV file with headers
+function csvJSON(csv) {
+
+  let lines = csv.split("\n");
+
+  let result = [];
+
+  let headers = lines[0].split(",");
+
+  for (let i = 1; i < lines.length; i++) {
+
+    let obj = {};
+    let currentline = lines[i].split(",");
+
+    for (let j = 0; j < headers.length; j++) {
+      obj[headers[j]] = currentline[j];
+    }
+
+    result.push(obj);
+
+  }
+
+  //return result; //JavaScript object
+  return JSON.stringify(result); //JSON
+}
+
+
+console.log(JSON.parse(csvJSON(file)));
