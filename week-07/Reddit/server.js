9a09fc7c3ba4f5417880ce39ce983aa282f44f43
Khamsinsc <senbce@gmail.com>
2019-03-09 23:07:49 +0100

    finished upvote downvote and delete

--- week-07/Reddit/server.js
+++ week-07/Reddit/server.js
@@ -25,31 +25,67 @@ app.get('/hello', (req, res) => {
 
 app.get('/posts', (req, res) => {
   connection.query(`SELECT * FROM posts`, (err, result) => {
-    err ? console.log(err) : res.send({
+    err ? console.log(err) : res.status(200).json({
       "posts": result
     });
   })
 })
 
 app.post('/posts', (req, res) => {
-  console.log(req.headers);
-  if ('username' in req.headers === true && req.headers["content-type"] === 'application/json'){
+  if ('username' in req.headers === true && req.headers["content-type"] === 'application/json') {
     let userNameQuery = ` \`owner\` = ${req.headers.username},`
-    let query = connection.query(`INSERT INTO posts SET ${userNameQuery} ?`, req.body, (err,result)=>{
-     /*  if(err){
+    connection.query(`INSERT INTO posts SET ${userNameQuery} ?`, req.body, (err, insertResult) => {
+      if (err) {
         console.error(err);
-        return;       
-      } */
-      console.log(query.sql,'sqlstring');
+        return;
+      } else {
+        connection.query(`select * FROM posts where id = LAST_INSERT_ID()`, (err, result) => {
+          if (err) {
+            console.error(err);
+            return;
+          } res.json(result);
+        })
+      }
     })
   }
-  console.log(req);
-  res.send(req.body);
-  /* postFile.posts.forEach(post => {
-    connection.query(`SELECT * FROM posts`, (err, result) => {
-      err ? console.log(err) : console.log(result);
+})
+
+app.put('/posts/:id/:vote', (req, res) => {
+  if (req.headers["content-type"] === 'application/json' && 'vote' in req.params) {
+    connection.query(`update posts set vote = vote ${req.params.vote === 'upvote' ? '+1' : req.params.vote === 'downvote' ? '-1' : ' '} where id = ${req.params.id}`, (err, result) => {
+      if (err) {
+        console.error(err);
+        return;
+      } else {
+        connection.query(`select * FROM posts where id = ${req.params.id}`, (err, result) => {
+          if (err) {
+            console.error(err);
+            return;
+          } res.json(result);
+        })
+      }
     })
-  }); */
+  }
+})
+
+app.delete('/posts/:id', (req, res) => {
+  if ('username' in req.headers === true && req.headers["content-type"] === 'application/json') {
+    let userNameQuery = ` \`owner\` = ${req.headers.username}`
+    connection.query(`select * FROM posts WHERE id = ${req.params.id} AND ${userNameQuery}`, (err, selectResult) => {
+      if (err) {
+        console.error(err);
+        return;
+      } let savedResult = selectResult;
+      connection.query(`DELETE FROM posts WHERE id = ${req.params.id} AND ${userNameQuery}`, (err, Result) => {
+        if (err) {
+          console.error(err);
+          return;
+        } else {
+          res.json(savedResult);
+        }
+      })
+    })
+  }
 })
 
 app.listen(PORT, () => {
