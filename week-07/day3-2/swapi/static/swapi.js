316592e0a5c456d430603206e2de8f8bd0e28608
James Tang <senbce@gmail.com>
2019-03-10 22:37:36 +0100

    finished swapi with jquery

--- week-07/day3-2/swapi/static/swapi.js
+++ week-07/day3-2/swapi/static/swapi.js
@@ -4,14 +4,55 @@ const button = document.querySelector('#button');
 const content = document.querySelector('#content');
 const httpRequest = new XMLHttpRequest();
 httpRequest.onreadystatechange = console.log;
-app.use(express.json());
+const LINK = 'https://swapi.co/api/people/?search='
 
-button.addEventListener('click', () => {
-   httpRequest.open('POST', '/search:' )
+$('#form').submit((e) => {
+  e.preventDefault();
+  let input = $('input#name').val();
 
+  $.ajax({
+    method: 'GET',
+    url: LINK + input,
+    dataType: 'json'
+  }).done((data) => {
+    $('.namelist').empty();
+    $('.movielist').empty();
+    let result = data.results;
+    renderHTML(result);
+  });
 })
 
-/* 
+const renderHTML = (data) => {
+  $('.namelist').empty();
+  for (let index = 0; index < data.length; index++) {
+    $('.namelist').append(`<li class="name">${data[index].name}</li>`);
+  }
+
+  $('.name').click((e) => {
+    $('.movielist').empty();
+    data.forEach((element, i) => {
+      if (element.name === e.target.innerText) {
+        data[i].films.forEach(e => {
+          $.ajax({
+            method: 'GET',
+            url: e,
+            dataType: 'json'
+          }).done((data) => {
+            renderMovieList(data);
+          })
+        })
+      };
+    });
+  })
+}
+
+const renderMovieList = (data) => {
+
+  $('.movielist').append(`<li class="movieinfo">${data.title}(${data.release_date})</li>`);
+}
+
+
+/*
   httpRequest.open('GET', 'https://api.nytimes.com/svc/search/v2/articlesearch.json?q=moon landing&api-key=C8xQ753V65ykkdc8625PRMKuAbQH5qq9', true);
 
   httpRequest.onload = () => {
@@ -29,4 +70,4 @@ const renderHTML = (data) => {
 }
  */
 
-httpRequest.send();
+/* httpRequest.send(); */
