a282593578594ece4042e46f50f4b1f574c28fb7
Khamsinsc <senbce@gmail.com>
2019-03-11 14:37:04 +0100

    finishedall

--- week-08/day1/js/main.js
+++ week-08/day1/js/main.js
@@ -1,12 +1,25 @@
-
 'use strict';
-
 const myDiv = document.querySelector('div');
 
 myDiv.addEventListener('left-side-click', (ev) => {
+  console.log(ev);
   ev.target.style.backgroundColor = 'yellow';
 });
 
 myDiv.addEventListener('right-side-click', (ev) => {
+  console.log(ev);
   ev.target.style.backgroundColor = 'blue';
 });
+
+const leftEvent = new CustomEvent('left-side-click');
+const rightEvent = new CustomEvent('right-side-click');
+
+myDiv.addEventListener('click', (e) => {
+  console.log(typeof e.layerX);
+  console.log(e);
+  if (e.layerX < 200) {
+    myDiv.dispatchEvent(leftEvent);
+  } else {
+    myDiv.dispatchEvent(rightEvent);
+  }
+})
