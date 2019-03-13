dcb612c1550decb618e987c301b16f1707efcfe9
Khamsinsc <senbce@gmail.com>
2019-03-13 17:17:17 +0100

    no bodyparser needed

--- week-08/day3/newsletter/server.js
+++ week-08/day3/newsletter/server.js
@@ -4,17 +4,8 @@ const express = require('express');
 const app = express();
 const path = require('path');
 const PORT = 8080;
-const mysql = require('mysql');
-const fs = require('fs');
-let postFile = JSON.parse(fs.readFileSync('./assets/posts.json', 'utf-8'));
-app.use(express.json());
 
-const connection = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
-  password: 'password',
-  database: 'reddit'
-})
+app.use(express.json());
 
 app.use(express.json());
 app.use('/assets', express.static('assets'));
