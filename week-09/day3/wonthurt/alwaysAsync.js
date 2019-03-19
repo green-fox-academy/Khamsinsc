5bc7d00fb94a7853bd0d974a578de344b33bf774
Khamsinsc <senbce@gmail.com>
2019-03-19 17:35:54 +0100

    always async done

---
+++ week-09/day3/wonthurt/alwaysAsync.js
@@ -0,0 +1,11 @@
+'use strict';
+
+require('es6-promise');
+
+let promise = new Promise((resolve, reject) => {
+  resolve('PROMISE VALUE');
+})
+  .then(console.log)
+
+  console.log('MAIN PROGRAM');
+
