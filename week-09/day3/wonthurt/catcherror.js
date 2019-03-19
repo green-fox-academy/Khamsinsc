7f26d6d56476d1c8709099185a1d7956ece7679b
James Tang <senbce@gmail.com>
2019-03-19 21:50:57 +0100

    finished errorcatcher

--- week-09/day3/wonthurt/catcherror.js
+++ week-09/day3/wonthurt/catcherror.js
@@ -2,3 +2,17 @@
 
 require('es6-promise');
 
+const parsePromised = () => {
+  return new Promise((resolve, reject) => {
+    try {
+      JSON.parse(process.argv[2]);
+    } catch (error) {
+      reject(error.message);
+    }
+    resolve(JSON.parse(process.argv[2]));
+  })
+}
+
+parsePromised()
+.then(console.log)
+.catch(console.log);
