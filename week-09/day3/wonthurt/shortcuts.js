df90f03f6a1a012c7c4ae8d883f1126edc06d83a
James Tang <senbce@gmail.com>
2019-03-19 20:55:23 +0100

    finished shortcuts

--- week-09/day3/wonthurt/shortcuts.js
+++ week-09/day3/wonthurt/shortcuts.js
@@ -1,1 +1,11 @@
 'use strict';
+
+require('es6-promise');
+
+let promise1 = Promise.resolve('Resolve message');
+
+let promise2 = Promise.reject(new Error('Error message'));
+
+promise1.then(console.log);
+
+promise2.catch(err=>console.log(err.message));
